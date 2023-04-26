2ae1d2835e3810c85b9571b3a6bef0e668eaac8a
Farhod <ubaydullaevfarhod18@gmail.com>
2023-04-26 22:46:55 +0500

    slider changes

--- components/home-two/home-two.js
+++ components/home-two/home-two.js
@@ -3,40 +3,75 @@ import s from './home-two.module.scss'
 import Link from 'next/link'
 
 const HomeTwo = () => {
-  const items = useMemo(() => [
-    {
-      link: '/catalog/svitshot-svitshot-detskij-zhenskaya-2-zhenskaya-zhenskaya',
-      title: 'Женские свитшоты с орнаментом',
-      imgUrl: '/home/home-two-2.jpg'
-    },
-    {
-      link: '/catalog/futbolki-futbolka-maternity-zhenskaya-futbolka-maternity-zhenskaya-futbolka-maternity-muzhskaya',
-      title: 'Мужские футболки с орнаментом',
-      imgUrl: '/home/home-two-1.jpg'
-    },
-    {
-      link: '/catalog/aksessuary',
-      title: 'Женские аксессуары',
-      imgUrl: '/home/home-two-3.webp'
-    },
-    {
-      link: '/catalog/dizajnerskie-skaterti',
-      title: 'Декор-текстиль с узорами',
-      imgUrl: '/home/home-two-4.jpg'
-    },
-  ], [])
+  const items = useMemo(
+    () => [
+      {
+        link: '/catalog/svitshot-svitshot-detskij-zhenskaya-2-zhenskaya-zhenskaya',
+        title: 'Женские футболки с орнаментом',
+        imgUrl: '/home/cover2.jpg',
+      },
+      {
+        link: '/catalog/futbolki-futbolka-maternity-zhenskaya-futbolka-maternity-zhenskaya-futbolka-maternity-muzhskaya',
+        title: 'Мужские футболки с орнаментом',
+        imgUrl: '/home/home-two-1.jpg',
+      },
+      {
+        link: '/catalog/aksessuary',
+        title: 'Женские аксессуары',
+        imgUrl: '/home/home-two-3.webp',
+      },
+      {
+        link: '/catalog/dizajnerskie-skaterti',
+        title: 'Декор-текстиль с узорами',
+        imgUrl: '/home/home-two-4.jpg',
+      },
+    ],
+    []
+  )
 
   return (
     <div className={s.wrapper}>
       <div className={s.innerWrapper}>
-        {items.map(el => (
-          <div className={s.block} style={{ backgroundImage: `url(${el.imgUrl})` }}>
-            <Link href={el.link}>
-              <a className={s.desc}>
+        {items.map((el, index) => (
+          <div
+            className={s.block}
+            style={{ backgroundImage: `url(${el.imgUrl})` }}
+          >
+            {index === 0 ? (
+              <div className={s.desc}>
                 <div className={s.text}>{el.title}</div>
-                <div className={s.link}>Купить</div>
-              </a>
-            </Link>
+                <div
+                  style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                    gap: '50px',
+                  }}
+                >
+                  <Link href={'/catalog/zhenskaya'}>
+                    <a>
+                      <div className={`${s.link} ${s.largeLink}`}>
+                        Для девушек
+                      </div>
+                    </a>
+                  </Link>
+                  <Link href={'/catalog/muzhskaya'}>
+                    <a>
+                      <div className={`${s.link} ${s.largeLink}`}>
+                        Для мужчин
+                      </div>
+                    </a>
+                  </Link>
+                </div>
+              </div>
+            ) : (
+              <Link href={el.link}>
+                <a className={s.desc}>
+                  <div className={s.text}>{el.title}</div>
+                  <div className={s.link}>Купить</div>
+                </a>
+              </Link>
+            )}
           </div>
         ))}
       </div>
